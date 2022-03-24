6b77cec2f1b80d8e531a6590d978e838ac6ea2c9
ang-jason <angjason896@gmail.com>
2022-03-24 20:04:47 +0800

    fixed gallery next TKS YUDA

--- src/App.js
+++ src/App.js
@@ -1,184 +1,93 @@
 import './App.css';
 import './index.css'
 import React, {useState , useEffect} from 'react'
-import { useQuery } from 'react-query'
+import { QueryClient, QueryClientProvider } from "react-query";
 import Navbar from './pages/navbar';
 import Gallery from './pages/gallery';
 import Watchlist from './pages/watchlist';
 
 
-export const GalleryContext=React.createContext()
 
-const apiKey = process.env.REACT_APP_NOT_SECRET_CODE
+export const GalleryContext = React.createContext();
 
-const fetchGallery = async (key,pagination) => {
-  // const pagination = (key.next === undefined) ? null : key.next
-  // console.log('pagenation', pagination)
-
-
-  // const pagination = (key.queryKey[1] === undefined) ? null : key
-
-  // console.log('pagenation33', pagination)
-  // console.log('key', key)
-
-  const options = {
-    method: 'GET',
-    headers: {Accept: 'application/json', 'X-API-KEY': apiKey}
-  };
-
-  // const res = await fetch(`https://api.opensea.io/api/v1/assets?order_direction=desc&offset=0&limit=20`, options)
-  // const res = await fetch(`https://api.opensea.io/api/v1/events?only_opensea=false&offset=${pagination}&limit=18`, options)
-  // const res = await fetch(`https://api.opensea.io/api/v1/events?cursor=${pagination}`, options)
-
-
-  const url = pagination ? `https://api.opensea.io/api/v1/events?cursor=${pagination}` : `https://api.opensea.io/api/v1/events?`
-  console.log('url',url)
-  const res = await fetch(url, options)
-
-  // const res = await fetch(`https://api.opensea.io/api/v1/events?only_opensea=false&offset=0&limit=20`,options)
-
-  // const res = await fetch('http://swapi.dev/api/planets/')
-  // only proceed once promise is resolved
-  let data = await res.json();
-  // only proceed once second promise is resolved
-  console.log("data",data)
-  return data;
-}
-
-const LOCALSTORAGEKEY = 'react-nft-gallery'
+const LOCALSTORAGEKEY = "react-nft-gallery";
+const queryClient = new QueryClient();
 
 function App() {
-  
-  const [page, setPage] = useState('gallery')
+  const [page, setPage] = useState("gallery");
 
-  const [pagination, setPagination] = useState(null)
+  const [watchList, setwatchList] = useState("[]");
 
-  // console.log('pagination',pagination)
-  const { data, status} = useQuery(['gallery',pagination], fetchGallery,{
-    staleTime:30000,
-    onSuccess: () => console.log('data fetched')
-  })
-  // console.log(data)
+  const saveToLocalStorage = (assets) => {
+    localStorage.setItem(LOCALSTORAGEKEY, JSON.stringify(assets));
+  };
 
-  const [watchList, setwatchList] = useState('[]')
+  const getFromLocalStorage = (assets) => {
+    const storageWatchList = JSON.parse(localStorage.getItem(LOCALSTORAGEKEY));
+    setwatchList(storageWatchList);
+  };
 
-
-  const nextPagination = () =>{
-
-    console.log('nexttt', data.next);
-    setPagination(data.next)
-  }
-
-  const prevPagination = () =>{
-    // console.log('prev', pagination)
-    console.log('prevv', data.previous);
-    // if (pagination != null ){
-    //   setPagination(data.previous)
-    // }
-    setPagination(data.previous)
-  }
-
-
-  const saveToLocalStorage = (assets) =>
-  {
-    localStorage.setItem(LOCALSTORAGEKEY, JSON.stringify(assets))
-  }
-  const getFromLocalStorage = (assets) =>
-  {
-    const storageWatchList = JSON.parse(localStorage.getItem(LOCALSTORAGEKEY))
-    // console.log(storageWatchList)
-    setwatchList(storageWatchList)
-  }
-
-
-  const findItemwatchList = (asset) =>{
-    // console.log('asset',asset)
-    console.log('findItemwatchList',asset.asset.id)
-    // console.log('watchList',watchList)
-    if (watchList !== null)
-    {
-      const isInside = watchList.find(item => item.asset.id === asset.asset.id)
-      // console.log('ininSide',isInside)
-      return isInside ? true : false
-    }else{
-      return false
+  const findItemwatchList = (asset) => {
+    if (watchList !== null) {
+      const isInside = watchList.find(
+        (item) => item.asset.id === asset.asset.id
+      );
+      return isInside ? true : false;
+    } else {
+      return false;
     }
-    // const isInside = watchList.some(item => item.asset.id === asset.asset.id)
-    // console.log('isInside',isInside)
-    // console.log('findItemwatchList',isInside ? true : false)
-    // if (isInside == null)
-    // {
-    //   return false
-    // }
-    // return isInside ? true : false
-  }
-
+  };
 
   const addwatchList = (asset) => {
-      console.log("adding!! @ addwatchList",asset.asset.id)
-      const isInside = findItemwatchList(asset)
-      // console.log('isInside',isInside)
-      // if (!isInside || isInside == null ){
-      // console.log("added!! @ addwatchList",watchList)
-      console.log()
-      if (!isInside){
-        if (watchList == null){
-          const newWatchList = [asset]
-          setwatchList(newWatchList)
-          saveToLocalStorage(newWatchList)
-          // console.log(newWatchList)
-        }else{
-          const newWatchList = [asset, ...watchList]
-          setwatchList(newWatchList)
-          saveToLocalStorage(newWatchList)
-          // console.log(newWatchList)
-        }
-  
+    const isInside = findItemwatchList(asset);
+    if (!isInside) {
+      if (watchList == null) {
+        const newWatchList = [asset];
+        setwatchList(newWatchList);
+        saveToLocalStorage(newWatchList);
+      } else {
+        const newWatchList = [asset, ...watchList];
+        setwatchList(newWatchList);
+        saveToLocalStorage(newWatchList);
       }
+    }
+  };
 
-      // }
-  }
-
-  const removewatchList = (asset) =>{
-      console.log("removing")
-      const newWatchList = watchList.filter((item)=> item.id !== asset.id )
-      if (newWatchList == null)
-      {
-        return
-      }
-      setwatchList(newWatchList)
-      saveToLocalStorage(newWatchList);
-  }
+  const removewatchList = (asset) => {
+    const newWatchList = watchList.filter((item) => item.id !== asset.id);
+    if (newWatchList == null) {
+      return;
+    }
+    setwatchList(newWatchList);
+    saveToLocalStorage(newWatchList);
+  };
 
   useEffect(() => {
-    getFromLocalStorage()
+    getFromLocalStorage();
     // for development work
     // localStorage.clear()
-  }, [])
+  }, []);
 
-
-
-  const galleryContextValue={
+  const galleryContextValue = {
     // key:value same text, just specific once
     addwatchList,
     removewatchList,
     findItemwatchList,
-    nextPagination,
-    prevPagination
-  }
-
-
+  };
 
   return (
-
-    <div>
-    <GalleryContext.Provider value={galleryContextValue}>
-      <Navbar setPage={setPage}/>
-      { page === 'gallery' ? <Gallery data={data} status={status} watchlist={watchList}/> : <Watchlist watchList={watchList}/>
-      }
-    </GalleryContext.Provider>
-    </div>
-
+    <QueryClientProvider client={queryClient}>
+      <div>
+        <GalleryContext.Provider value={galleryContextValue}>
+          <Navbar setPage={setPage} />
+          {page === "gallery" ? (
+            <Gallery watchlist={watchList} />
+          ) : (
+            <Watchlist watchList={watchList} />
+          )}
+        </GalleryContext.Provider>
+      </div>
+    </QueryClientProvider>
   );
 }
 
--- src/pages/gallery.jsx
+++ src/pages/gallery.jsx
@@ -1,64 +1,105 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
+import { useQuery } from "react-query";
 import { GalleryContext } from 'App'
 import {Card} from 'domains/gallery'
 import { Button } from 'components/button'
 import Loader from 'components/loader'
-// import Search from 'components/search'
 
+const apiKey = process.env.REACT_APP_NOT_SECRET_CODE
 
-export default function Gallery({data, status,watchlist}) {
-    // console.log(data,status)
-    const {     
-        addwatchList,
-        removewatchList,
-        findItemwatchList,
-        nextPagination,
-        prevPagination
-    } = useContext(GalleryContext)
+const fetchGallery = async (pagination) => {
+    const options = {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "X-API-KEY": apiKey,
+      },
+    };
+  
+    const url = pagination
+      ? `https://api.opensea.io/api/v1/events?cursor=${pagination}`
+      : `https://api.opensea.io/api/v1/events?`;
+  
+    return await fetch(url, options).then((response) => {
+        
 
+        return (response.json())
+        
+    });
+  };
+  
+  export default function Gallery({ watchlist }) {
+    const { addwatchList, removewatchList, findItemwatchList } =
+      useContext(GalleryContext);
+  
+    const [pagination, setPagination] = useState(null);
+  
+    const { data, status } = useQuery(["gallery", pagination], () =>
+      fetchGallery(pagination)
+    );
+  
+    console.log(data)
 
+    const nextPagination = () => {
+        console.log('next',data.next)
+      if (data.next != null) {
+        setPagination(data.next);
+      }
+    };
+  
+    const prevPagination = () => {
+        console.log('previous',data.previous)      
+      if (data.previous != null) {
+        setPagination(data.previous);
+      }
+    };
+  
     return (
-        <div>
-
-            {/* <Search/> */}
-            
-    
-            <div className="flex content-center items-center justify-center mt-6">
-                <div className="flex gap-4">     
-                
-                <Button variant="outline" onClick={prevPagination}>PREV</Button>
-                <Button variant="outline" onClick={nextPagination}>NEXT</Button>
-                
-                </div>
-               
-            </div>
-
-                {status === 'loading' && (<Loader/>)
-            }
-            {status === 'error' && (<div>Error fetching data</div>)
-            }
-            {status === 'success' && 
-
-            (   
-                <div className="overflow-y-hidden">
-       
-                <div className="grid grid-cols-3 m-5 2xl:grid-cols-5 gap-1 2xl:m-2 2xl:p-4">
-                {data.asset_events.map(asset =>{
-                    return <Card 
-                    key={asset.id} 
-                    asset={asset} 
-                    watchlist={watchlist}
-                    handleRemoveListClick={()=>removewatchList(asset)}
-                    handleAddListClick={()=>{addwatchList(asset)}}
-                    handlefindItem={()=>findItemwatchList(asset)}
-
-                     />
-
-                }
-                )}</div>
-                </div>
-            )
-            }
+      <div>
+        {/* <Search/> */}
+  
+        <div className="flex content-center items-center justify-center mt-6">
+          <div className="flex gap-4">
+            <Button
+              variant="outline"
+              onClick={prevPagination}
+              disabled={status === "loading"}
+            >
+              PREV
+            </Button>
+            <Button
+              variant="outline"
+              onClick={nextPagination}
+              disabled={status === "loading"}
+            >
+              NEXT
+            </Button>
+          </div>
         </div>
-    )
-}
+  
+        {status === "loading" && <Loader />}
+        {status === "error" && <div>Error fetching data</div>}
+        {status === "success" && (
+          <div className="overflow-y-hidden">
+            <div className="grid grid-cols-3 m-5 2xl:grid-cols-5 gap-1 2xl:m-2 2xl:p-4">
+              {data.asset_events?.map((asset) => {
+                return (
+                  <Card
+                    key={asset.id}
+                    asset={asset}
+                    watchlist={watchlist}
+                    handleRemoveListClick={() => removewatchList(asset)}
+                    handleAddListClick={() => {
+                      addwatchList(asset);
+                    }}
+                    handlefindItem={() => findItemwatchList(asset)}
+                  />
+                );
+              })}
+            </div>
+          </div>
+        )}
+      </div>
+    );
+  }
+  
